ef7a91e5461a6f2d6a66e0bb252adde0b28d3aae
ChinmayJoshi1412 <chinmayjoshi1412@gmail.com>
2022-11-12 20:21:17 +0530

    added songs

--- lib/main.dart
+++ lib/main.dart
@@ -22,195 +22,261 @@ class _LP_spotifyState extends State<LP_spotify> {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Stack(
-            children: [
-              Container(
-                child:Image.asset('assets/LPBG.jfif'),
-              ),
-              Container(
-                decoration:BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.bottomCenter,
-                    end: Alignment.topCenter,
-                    colors: [
-                      Colors.black12.withOpacity(1),
-                      Colors.black12,
-                      Colors.black12,
-                      Colors.black12,
-                      Colors.black12
-                    ]
-                  )
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Stack(
+              children: [
+                Container(
+                  child:Image.asset('assets/LPBG.jfif'),
                 ),
-                height: 400,
-                width: 400,
-                alignment: Alignment.bottomLeft,
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(20,0,0,0),
-                child: Container(
-                  child: Text('Linkin Park',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 40
-                  ),),
-                  height: 380,
+                Container(
+                  decoration:BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.bottomCenter,
+                      end: Alignment.topCenter,
+                      colors: [
+                        Colors.black12.withOpacity(1),
+                        Colors.black12,
+                        Colors.black12,
+                        Colors.black12,
+                        Colors.black12
+                      ]
+                    )
+                  ),
+                  height: 400,
+                  width: 400,
                   alignment: Alignment.bottomLeft,
                 ),
-              )
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
-            child: Container(
-              height: 400,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('25,627,026 monthly listeners',
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(15,0,0,0),
+                  child: Container(
+                    child: Text('Linkin Park',
                     style: TextStyle(
-                    fontSize: 15,
-                      color: Colors.grey,
-                  ),),
-                  SizedBox.square(dimension: 10),
-                  Row(
-                    children: [
-                      OutlinedButton(onPressed: (){}, child: Text('Following',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 15
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 40
+                    ),),
+                    height: 380,
+                    alignment: Alignment.bottomLeft,
+                  ),
+                )
+              ],
+            ),
+
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+              child: Container(
+                alignment: Alignment.topLeft,
+                child: Text('25,627,026 monthly listeners',
+                          style: TextStyle(
+                          fontSize: 15,
+                            color: Colors.grey,
                         ),),
-                          style: OutlinedButton.styleFrom(
-                              side: BorderSide(width: 1,color: Colors.white)
-                          )
+              ),
+                    ),
+                    SizedBox.square(dimension: 10),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+                child: Row(
+                  children: [
+                    OutlinedButton(onPressed: (){}, child: Text('Following',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15
+                      ),),
+                        style: OutlinedButton.styleFrom(
+                            side: BorderSide(width: 1,color: Colors.white)
+                        )
+                    ),
+                    SizedBox.square(dimension: 30),
+                    Icon(Icons.more_vert,color: Colors.grey,),
+                    SizedBox(width: 120),
+                    Icon(Icons.shuffle,color: Colors.grey,),
+                    SizedBox(width: 20),
+                    Container(
+                      child: FloatingActionButton(
+                        onPressed: (){},
+                        backgroundColor: Colors.greenAccent[700],
+                        child: Icon(Icons.play_arrow,color: Colors.black,size: 30,),
                       ),
-                      SizedBox.square(dimension: 30),
-                      Icon(Icons.more_vert,color: Colors.grey,),
-                      SizedBox(width: 120),
-                      Icon(Icons.shuffle,color: Colors.grey,),
-                      SizedBox(width: 20),
-                      Container(
-                        child: FloatingActionButton(
-                          onPressed: (){},
-                          backgroundColor: Colors.greenAccent[700],
-                          child: Icon(Icons.play_arrow,color: Colors.black,size: 30,),
-                        ),
-                      )
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+              child: Container(
+                alignment: Alignment.topLeft,
+                child: Text('Popular',
+                    style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20
+                )),
+              ),
+                    ),
+                    SizedBox(height: 20,),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+              child: Row(
+                  children: [
+                    Text('1',style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15
+                    )),
+                    SizedBox(width: 20,),
+                    Image(image: AssetImage('assets/intheend.jfif'),height: 50,width: 50,),
+                    SizedBox(width: 15,),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('In the end',style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16
+                        ),),
+                        SizedBox(height:5 ),
+                        Text('1,377,302,928',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.grey,
+                          ),)
+                      ],
+                    ),
+                    SizedBox(width: 150),
+                    Icon(Icons.more_vert,color: Colors.grey,)
+                  ],
+                ),
+                    ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 20, 0, 0),
+              child: Row(
+                  children: [
+                    Text('2',style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15
+                    )),
+                    SizedBox(width: 20,),
+                    Image(image: AssetImage('assets/numb.jfif'),height: 50,width: 50,),
+                    SizedBox(width: 15,),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Numb',style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16
+                        ),),
+                        SizedBox(height:5 ),
+                        Text('1,139,914,931',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.grey,
+                          ),)
+                      ],
+                    ),
+                    SizedBox(width: 150),
+                    Icon(Icons.more_vert,color: Colors.grey,)
+                  ],
+                ),
+              ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 20, 0, 0),
+              child: Row(
+                  children: [
+                    Text('3',style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15
+                    )),
+                    SizedBox(width: 20,),
+                    Image(image: AssetImage('assets/whativedone.jfif'),height: 50,width: 50,),
+                    SizedBox(width: 15,),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('What Ive Done',style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16
+                        ),),
+                        SizedBox(height:5 ),
+                        Text('1,377,302,928',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.grey,
+                          ),)
+                      ],
+                    ),
+                    SizedBox(width: 140),
+                    Icon(Icons.more_vert,color: Colors.grey,)
+                  ],
+                ),
+              ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 20, 0, 0),
+              child: Row(
+                children: [
+                  Text('4',style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15
+                  )),
+                  SizedBox(width: 20,),
+                  Image(image: AssetImage('assets/numbencore.jpg'),height: 50,width: 50,),
+                  SizedBox(width: 15,),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Numb/Encore',style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16
+                      ),),
+                      SizedBox(height:5 ),
+                      Text('749,408,608',
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.grey,
+                        ),)
                     ],
                   ),
-                  SizedBox(height: 20),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text('Popular',style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20
-                    )),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Row(
-                      children: [
-                        Text('1',style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 15
-                        )),
-                        SizedBox(width: 20,),
-                        Image(image: AssetImage('assets/intheend.jfif'),height: 50,width: 50,),
-                        SizedBox(width: 15,),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('In the end',style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16
-                            ),),
-                            SizedBox(height:5 ),
-                            Text('1,377,302,928',
-                              style: TextStyle(
-                                fontSize: 15,
-                                color: Colors.grey,
-                              ),)
-                          ],
-                        ),
-                        SizedBox(width: 150),
-                        Icon(Icons.more_vert,color: Colors.grey,)
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Row(
-                      children: [
-                        Text('2',style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 15
-                        )),
-                        SizedBox(width: 20,),
-                        Image(image: AssetImage('assets/numb.jfif'),height: 50,width: 50,),
-                        SizedBox(width: 15,),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('Numb',style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 16
-                            ),),
-                            SizedBox(height:5 ),
-                            Text('1,139,914,931',
-                              style: TextStyle(
-                                fontSize: 15,
-                                color: Colors.grey,
-                              ),)
-                          ],
-                        ),
-                        SizedBox(width: 150),
-                        Icon(Icons.more_vert,color: Colors.grey,)
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Row(
-                      children: [
-                        Text('3',style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 15
-                        )),
-                        SizedBox(width: 20,),
-                        Image(image: AssetImage('assets/whativedone.jfif'),height: 50,width: 50,),
-                        SizedBox(width: 15,),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('What Ive Done',style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 16
-                            ),),
-                            SizedBox(height:5 ),
-                            Text('1,377,302,928',
-                              style: TextStyle(
-                                fontSize: 15,
-                                color: Colors.grey,
-                              ),)
-                          ],
-                        ),
-                        SizedBox(width: 140),
-                        Icon(Icons.more_vert,color: Colors.grey,)
-                      ],
-                    ),
-                  ),
-
+                  SizedBox(width: 140),
+                  Icon(Icons.more_vert,color: Colors.grey,)
                 ],
               ),
-              alignment: Alignment.topLeft,
             ),
-          )
-        ],
-      ),
-    );
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 20, 0, 0),
+              child: Row(
+                children: [
+                  Text('5',style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15
+                  )),
+                  SizedBox(width: 20,),
+                  Image(image: AssetImage('assets/intheend.jfif'),height: 50,width: 50,),
+                  SizedBox(width: 15,),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('One Step Closer',style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16
+                      ),),
+                      SizedBox(height:5 ),
+                      Text('422,484,330',
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.grey,
+                        ),)
+                    ],
+                  ),
+                  SizedBox(width: 140),
+                  Icon(Icons.more_vert,color: Colors.grey,)
+                ],
+              ),
+            ),
+            ],
+          ),
+        ),
+      );
   }
 }
